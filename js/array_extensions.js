1305aa12caac352409863b2a19d78b2d8642c15d
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-28 17:02:04 -1000

    added spec, index and array_extensions

---
+++ js/array_extensions.js
@@ -0,0 +1,16 @@
+// Array Extensions file for Array 
+
+//toString method
+//must take the contents of an array and create a string
+Array.prototype.toString = function(first_argument) {
+      // body...
+};
+    
+Array.prototype.eachItem = function(first_argument) {
+      // body...
+};
+    
+Array.prototype.extra = function(first_argument) {
+      // body...
+};
+    
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,12 +1,37 @@
-describe('array_ext', function () {
+describe('Array toString Prototype', function () {
   
-  describe('Must be a function', function () {
-  it("should show something in the index.html when viewed in browser", function() {
-    expect(true).to.be.equal(true);
+  it("Array toString.prototype must be a function", function() {
+     // expect( Array.prototype.toString ).to.be.a('function');
+     expect(Array.prototype.toString).to.be.a('function'); 
+     // expect(true).to.be.equal(true);  
   });
 
-});
+  it("Array toString.prototype must be a function", function() {
+       // expect( Array.prototype.toString ).to.be.a('function');
+       expect(Array.prototype.toString).to.be.instanceOf(Function); 
+       // expect(true).to.be.equal(true);  
+    });
+
+});//ends the array describe 
 
 
-});
+describe('Array forEach Prototype', function () {
+  
+  it("Array.forEach.prototype must be a function", function() {
+     // expect( Array.prototype.toString ).to.be.a('function');
+     expect(Array.prototype.eachItem).to.be.a('function'); 
+     // expect(true).to.be.equal(true);  
+  });
+
+});//ends the array describe 
+
+describe('Array Extra Prototype', function () {
+  
+  it("Array.Extra.prototype must be a function", function() {
+     // expect( Array.prototype.toString ).to.be.a('function');
+     expect(Array.prototype.eachItem).to.be.a('function'); 
+     // expect(true).to.be.equal(true);  
+  });
+
+});//ends the array describe 
 
