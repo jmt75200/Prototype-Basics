92789c4ae5912cd7a595627100b4f2b688254a5f
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-28 19:31:34 -1000

    adding first array test and function

--- js/array_extensions.js
+++ js/array_extensions.js
@@ -2,13 +2,20 @@
 
 //toString method
 //must take the contents of an array and create a string
-Array.prototype.toString = function(first_argument) {
-      // body...
+Array.prototype.toString = function( someArray ) {
+      return someArray.join(',');
 };
+
+//forEach method
+//must take a function as an argument and call that function against each element in the array
+//recommended to use a callback 
     
 Array.prototype.eachItem = function(first_argument) {
       // body...
 };
+
+//Extra method
+//must take an array function written in ArrayFun and add them to the Array.prototype
     
 Array.prototype.extra = function(first_argument) {
       // body...
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -6,11 +6,20 @@ describe('Array toString Prototype', function () {
      // expect(true).to.be.equal(true);  
   });
 
-  it("Array toString.prototype must be a function", function() {
+  it("Must pass in one argument ", function() {
        // expect( Array.prototype.toString ).to.be.a('function');
-       expect(Array.prototype.toString).to.be.instanceOf(Function); 
-       // expect(true).to.be.equal(true);  
+       var test_array = ["puppy", "kitten", "pandas"];
+       expect(test_array).to.be.an('array');
+       expect(test_array).not.to.be.null();
     });
+
+  it("Result must be a string", function() {
+         // expect( Array.prototype.toString ).to.be.a('function');
+         var result = '"puppy", "kitten", "pandas"';
+         
+         expect(result).to.be.an('string');
+         expect(result).not.to.be.null();
+      });
 
 });//ends the array describe 
 
@@ -23,15 +32,26 @@ describe('Array forEach Prototype', function () {
      // expect(true).to.be.equal(true);  
   });
 
+  // it("Must pass in one argument", function() {
+  //      // expect( Array.prototype.toString ).to.be.a('function');
+  //      var test_array = ["puppy", "kitten", "pandas"];
+       
+  //      expect(test_array).to.be.an('array');
+  //      expect(test_array).not.to.be.null();
+
+  //   });
+
 });//ends the array describe 
 
 describe('Array Extra Prototype', function () {
   
   it("Array.Extra.prototype must be a function", function() {
      // expect( Array.prototype.toString ).to.be.a('function');
-     expect(Array.prototype.eachItem).to.be.a('function'); 
+     expect(Array.prototype.extra).to.be.a('function'); 
      // expect(true).to.be.equal(true);  
   });
+
+
 
 });//ends the array describe 
 
