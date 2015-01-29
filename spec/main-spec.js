5744b7fc86f34abe42743638bd8cd3bb1ee249bd
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-28 16:12:52 -1000

    renaming and moving stuff

---
+++ spec/main-spec.js
@@ -0,0 +1,12 @@
+describe('array_ext', function () {
+  
+  describe('Must be a function', function () {
+  it("should show something in the index.html when viewed in browser", function() {
+    expect(true).to.be.equal(true);
+  });
+
+});
+
+
+});
+
