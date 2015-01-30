fbc387ef48ed1d45329907658444032b772f5330
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-29 14:32:11 -1000

    adding more to spec test, html and running implimentation

--- js/array_extensions.js
+++ js/array_extensions.js
@@ -2,22 +2,26 @@
 
 //toString method
 //must take the contents of an array and create a string
-Array.prototype.toString = function( someArray ) {
-      return someArray.join(',');
+Array.prototype.toStrang = function( ) {
+      return this.join(", ");
+      
 };
 
 //forEach method
 //must take a function as an argument and call that function against each element in the array
 //recommended to use a callback 
     
-Array.prototype.eachItem = function(first_argument) {
-      // body...
+Array.prototype.eachItem = function( cb ) {
+  for (var i = 0; i < this.length; i++) {
+    cb(this[i]);
+  }
+   
 };
 
-//Extra method
-//must take an array function written in ArrayFun and add them to the Array.prototype
+// //Extra method
+// //must take an array function written in ArrayFun and add them to the Array.prototype
     
-Array.prototype.extra = function(first_argument) {
-      // body...
-};
+// Array.prototype.extra = function(first_argument) {
+//       // body...
+// };
     
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,57 +1,118 @@
-describe('Array toString Prototype', function () {
+describe('Array toStrang Prototype: toString', function () {
   
-  it("Array toString.prototype must be a function", function() {
-     // expect( Array.prototype.toString ).to.be.a('function');
-     expect(Array.prototype.toString).to.be.a('function'); 
-     // expect(true).to.be.equal(true);  
+  it("Array.prototype.toStrang must be a function", function() {
+     
+     // expect(Array.prototype.toStrang).to.be.a('function');
+     expect(Array.prototype.toStrang).to.be.a('function'); 
+      
   });
 
-  it("Must pass in one argument ", function() {
-       // expect( Array.prototype.toString ).to.be.a('function');
-       var test_array = ["puppy", "kitten", "pandas"];
-       expect(test_array).to.be.an('array');
-       expect(test_array).not.to.be.null();
+  it("Must turn an array into a string ", function() {
+       
+    var test_array = ["puppy", "kitten", "pandas"];
+    var result = test_array.toStrang();
+       
+    expect(test_array).to.be.an('array');
+    expect(test_array).not.to.be.null();
+    expect(result).to.have.string("puppy, kitten, pandas");
+  });
+
+});//ends the array describe 
+
+
+describe('Array eachItem Prototype: forEach', function () {
+  
+  it("Array.eachItem.prototype must be a function", function() {
+
+     expect(Array.prototype.eachItem).to.be.a('function'); 
+     
+  });
+
+  it("Return eachItem from test_array into result Array", function() {
+    
+    var test_array = [1,2,3,4];
+    var result = [];
+    
+    //testing for the call back value
+    test_array.eachItem(function(value){
+      result.push(value);
     });
 
-  it("Result must be a string", function() {
-         // expect( Array.prototype.toString ).to.be.a('function');
-         var result = '"puppy", "kitten", "pandas"';
-         
-         expect(result).to.be.an('string');
-         expect(result).not.to.be.null();
-      });
+    expect(test_array).to.be.an('array');
+
+    expect(result).to.eql(test_array);
+
+  });
+
 
 });//ends the array describe 
 
-
-describe('Array forEach Prototype', function () {
+// describe('Array Extra Prototype', function () {
   
-  it("Array.forEach.prototype must be a function", function() {
-     // expect( Array.prototype.toString ).to.be.a('function');
-     expect(Array.prototype.eachItem).to.be.a('function'); 
-     // expect(true).to.be.equal(true);  
-  });
+//   it("Array.Extra.prototype must be a function", function() {
+     
+//      expect(Array.prototype.extra).to.be.a('function'); 
+    
+//   });
 
-  // it("Must pass in one argument", function() {
-  //      // expect( Array.prototype.toString ).to.be.a('function');
-  //      var test_array = ["puppy", "kitten", "pandas"];
+//   it("Must pass in one argument ", function() {
+//      var test_array = [];
+//      expect(Array.prototype.extra).to.be.a('function'); 
        
-  //      expect(test_array).to.be.an('array');
-  //      expect(test_array).not.to.be.null();
+//   });
 
-  //   });
+
+
+// });//ends the array describe 
+
+// ------------- [String Prototypes] -------------- //
+
+describe('WTF Prototype', function () {
+  
+  it("WTF.prototype must be a function", function() {
+     
+     expect(String.prototype.WTF).to.be.a('function'); 
+      
+  });
+
+  it("Must return value 'WTF' for any given value ", function() {
+       
+    var test_array = "squirl";
+    var result = test_array.WTF();
+       
+    expect(test_array).to.be.a('string');
+    expect(result).to.equal("WTF");
+    });
 
 });//ends the array describe 
 
-describe('Array Extra Prototype', function () {
+
+describe('String Scramble Prototype', function () {
   
-  it("Array.Extra.prototype must be a function", function() {
-     // expect( Array.prototype.toString ).to.be.a('function');
-     expect(Array.prototype.extra).to.be.a('function'); 
-     // expect(true).to.be.equal(true);  
+  it("Scramble.prototype must be a function", function() {
+     
+     expect(String.prototype.scramble).to.be.a('function'); 
+     
+  });
+
+});//ends the array describe 
+
+describe('String Trim Prototype', function () {
+  
+  it("String.trim.prototype must be a function", function() {
+     
+     expect(String.prototype.trim).to.be.a('function'); 
+    
+  });
+
+  it("Must pass in one string argument ", function() {
+    var test_str = " Break Away ";
+
+    expect(test_str).to.be.a('string');
+    expect(test_str).to.not.be.null();
+       
   });
 
 
 
 });//ends the array describe 
-
